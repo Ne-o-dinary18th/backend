9188f2088ae2f4d9e5b029c26763e4b9ba7be7ee
kcw9609 <kang960953@gmail.com>
2025-11-23 05:23:51 +0900

    Exception: 태그 예외코드 추가

--- src/main/java/com/neordinary/global/apiPayload/code/status/ErrorStatus.java
+++ src/main/java/com/neordinary/global/apiPayload/code/status/ErrorStatus.java
@@ -11,10 +11,14 @@ import org.springframework.http.HttpStatus;
 public enum ErrorStatus implements BaseErrorCode {
 
     REPORT_NOT_FOUND(HttpStatus.NOT_FOUND, "4041", "보고서를 찾을 수 없습니다."),
-    TAG_NOT_FOUND(HttpStatus.NOT_FOUND, "4042", "태그를 찾을 수 없습니다."),
     RECEIPT_NOT_FOUND(HttpStatus.NOT_FOUND, "4043", "영수증을 찾을 수 없습니다."),
 
-    INTERNAL_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "5000", "서버 오류가 발생했습니다.");
+    INTERNAL_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "5000", "서버 오류가 발생했습니다."),
+
+    TAG_CREATE_ERROR(HttpStatus.NOT_FOUND,"TAG4001", "태그 생성에 실패했습니다."),
+    TAG_NOT_FOUND(HttpStatus.NOT_FOUND,"TAG4002", "해당 태그가 없습니다."),
+    TAG_NAME_REQUIRED(HttpStatus.NOT_FOUND, "TAG4003", "태그명은 필수 입니다."),
+    TAG_NAME_ALREADY_EXISTS(HttpStatus.NOT_FOUND, "TAG4004", "해당 태그명은 이미 존재합니다.");
 
     private final HttpStatus httpStatus;
     private final String code;
