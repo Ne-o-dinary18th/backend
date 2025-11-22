2a83a552cdb9b24d7490b52acad75ec81a30541a
huggywuggy1289 <153150703+huggywuggy1289@users.noreply.github.com>
2025-11-23 00:40:21 +0900

    :gear: cicd 충돌 해결

--- src/main/java/com/neordinary/domain/receipt/controller/ReceiptController.java
+++ src/main/java/com/neordinary/domain/receipt/controller/ReceiptController.java
@@ -3,7 +3,6 @@ package com.neordinary.domain.receipt.controller;
 import com.neordinary.domain.receipt.dto.req.ReceiptRequest;
 import com.neordinary.domain.receipt.Receipt;
 import com.neordinary.domain.receipt.dto.res.ReceiptResponse;
-import com.neordinary.domain.receipt.exception.code.ReceiptCode;
 import com.neordinary.domain.receipt.service.command.ReceiptCommandService;
 import com.neordinary.domain.receipt.service.query.ReceiptQueryService;
 import com.neordinary.global.apiPayload.ApiResponse;
